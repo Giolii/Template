d650fa8c8d2515bf0038ce2242f97643d55e9c31
Gi <160989051+Giolii@users.noreply.github.com>
2024-07-24 19:12:39 -0700

    Deleted old files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
 import "./style.css";
-import mod1 from "./mod1.js";
-import mod2 from "./mod2.js";
-
-const content = document.querySelector("#content");
-const mod21 = document.querySelector(".mod2");
-const mod11 = document.querySelector(".mod1");
-mod21.addEventListener("click", () => {
-  mod2();
-});
-mod11.addEventListener("click", () => {
-  mod1();
-});
-content.textContent = "ciao";
