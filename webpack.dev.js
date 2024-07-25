8bb8078ba746c9af5a25d5a3c69e00524fed15ed
Gi <160989051+Giolii@users.noreply.github.com>
2024-07-24 19:10:50 -0700

    Installed ESLint

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,24 +1,22 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const path = require('path');
-const webpack = require('webpack');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: "inline-source-map",
   devServer: {
-    static: path.join(__dirname, 'dist'),
+    static: path.join(__dirname, "dist"),
     // static: './dist',
-    hot: true,
-    watchFiles: ["src/*.html"],
+    // hot: true,
+    watchFiles: ["src/*.html", "src/*.css", "src/*.js"],
     // open:true,
     // compress:true,
-    // liveReload:true,
+    liveReload: false,
   },
-  plugins: [
-    // new webpack.HotModuleReplacementPlugin(),
-  ],
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   optimization: {
-    runtimeChunk: 'single',
+    runtimeChunk: "single",
   },
 });
