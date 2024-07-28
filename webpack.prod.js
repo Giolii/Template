4eb56fd5b3cc091f707e19ee9502ab912e8af06e
Gi <160989051+Giolii@users.noreply.github.com>
2024-07-28 13:26:52 -0700

    babel

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,20 +1,34 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const path = require('path');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const path = require("path");
 
 module.exports = merge(common, {
-  mode: 'production',
-  devtool: 'source-map',
+  mode: "production",
+  devtool: "source-map",
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   optimization: {
-    runtimeChunk: 'single',
-    moduleIds: 'deterministic',
+    runtimeChunk: "single",
+    moduleIds: "deterministic",
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
     },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
   },
 });
